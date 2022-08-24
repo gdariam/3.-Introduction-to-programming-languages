8d32b91747d6bc52da632b2ff5ce566be5c72486
Daria G <d.m.gorbacheva@gmail.com>
2022-08-24 18:54:42 +0300

    SEM_6HW

--- SEM_6/TASK_41 HOMEWORK/Program.cs
+++ SEM_6/TASK_41 HOMEWORK/Program.cs
@@ -3,5 +3,33 @@
 // -1, -7, 567, 89, 223-> 3
 
 Console.Clear();
-Console.WriteLine("Введите числа: ");
-string numbers = Console.ReadLine();
+
+Console.WriteLine("Введите количество чисел: ");
+int amount = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+
+int[] GetUserInput(int amount)
+{
+    int[] array = new int[amount];
+    for (int i = 0; i < amount; i++)
+    {
+        Console.WriteLine("Введите число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        if (array[i] > 0) { count++; }
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write(array[i] + ", ");
+        else Console.Write(array[i]);
+    }
+}
+
+int[] array = GetUserInput(amount);
+PrintArray(array);
+Console.Write(" -> ");
+Console.Write(count);
